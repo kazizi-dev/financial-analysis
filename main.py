98d455d5f31190c1e90958ca8fe077452c791cdf
kazizi-dev <kouroshazizi2012@gmail.com>
2021-08-03 02:56:12 -0700

    calculate options volume for given a range of strike prices

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import yfinance as yf
 from pprint import pprint
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
+from itertools import groupby
 
 """
     helps to understand the market behaviour from volume as strike price changes.
@@ -14,6 +15,19 @@ def graph_strike_over_volume(df, option_date, type):
     plt.ylabel('volume')
     plt.savefig(f'{type}.png')
     plt.close()
+
+def calculate_vol_for_strike_range(df):
+    range = dict()
+
+    for (key, group) in groupby(df['strike'], key=lambda x: x // 100):
+        volume = 0
+        for val in group:
+            volume += df[df['strike'] == val]['volume'].sum()
+        
+        range[key*100] = int(volume)
+
+    return range
+        
 
 """
     calls other functions to make graphs.
@@ -31,11 +45,19 @@ def option_analysis(TICKER_NAME, COLUMNS):
     puts = puts[COLUMNS]
     graph_strike_over_volume(puts, option_date, 'puts')
 
+    return {
+        'ticker': TICKER_NAME,
+        'put-call-vol-ratio': (puts['volume'].sum()/calls['volume'].sum()).round(2),
+        'call-volume-range': calculate_vol_for_strike_range(calls),
+        'put-volume-range': calculate_vol_for_strike_range(puts)
+    }
+
 
 if __name__ == '__main__':
     COLUMNS = ['strike', 'bid', 'ask', 'volume', 'openInterest','impliedVolatility']
     TICKER_NAME = 'TSLA'
-    option_analysis(TICKER_NAME, COLUMNS)
+    
+    pprint(option_analysis(TICKER_NAME, COLUMNS))
 
 
 
