aee4ff3c7d8171b2cce021a3dbddcc07098a73f3
kazizi-dev <kouroshazizi2012@gmail.com>
2021-08-04 11:10:16 -0700

    restructure code

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 # import seaborn as sns
 from itertools import groupby
+import os
 
 """
-    helps to understand the market behaviour from volume as strike price changes.
+    graph the data from two columns to understand market behaviour.
 """
 def graph_x_and_y_cols(df, option_date, cols, type):
     name = 'calls' if 'calls' in type else 'puts'
@@ -14,18 +15,20 @@ def graph_x_and_y_cols(df, option_date, cols, type):
     plt.title(f'[{name}] expiring on {option_date}')
     plt.xlabel(cols[0])
     plt.ylabel(cols[1])
-    plt.savefig(f'{type}.png')
+    plt.savefig(f'{option_date}-{type}.png')
     plt.close()
 
-def calculate_vol_for_strike_range(df):
-    range = dict()
 
-    for (key, group) in groupby(df['strike'], key=lambda x: x // 100):
+"""
+    calculate volume for different strike price range.
+"""
+def calculate_vol_for_strike_range(df, BREAKDOWN_RANGE):
+    range = dict()
+    for (key, group) in groupby(df['strike'], key=lambda x: x // BREAKDOWN_RANGE):
         volume = 0
         for val in group:
             volume += df[df['strike'] == val]['volume'].sum()
-        
-        range[key*100] = int(volume)
+        range[key * BREAKDOWN_RANGE] = int(volume)
 
     return range
         
@@ -33,51 +36,32 @@ def calculate_vol_for_strike_range(df):
 """
     calls other functions to perform operations.
 """
-def option_analysis(TICKER_NAME, COLUMNS):
-    stock = yf.Ticker(TICKER_NAME)
-    option_date = stock.options[0]
-    opt = stock.option_chain(option_date)
+def option_analysis(option_date, opt, TICKER_NAME, COLUMNS):
     calls = opt.calls
     calls = calls[COLUMNS]
+    graph_x_and_y_cols(calls, option_date, ['strike', 'impliedVolatility'], 'calls-iv')
+    graph_x_and_y_cols(calls, option_date, ['strike', 'volume'], 'calls-volume')
 
     puts = opt.puts
     puts = puts[COLUMNS]
-
-    
-    # the columns to use for graphs
-    cols = ['strike', 'impliedVolatility']
-    graph_x_and_y_cols(calls, option_date, cols, 'calls-iv')
-    graph_x_and_y_cols(puts, option_date, cols, 'puts-iv')
-
-    cols = ['strike', 'volume']
-    graph_x_and_y_cols(calls, option_date, cols, 'calls-volume')
-    graph_x_and_y_cols(puts, option_date, cols, 'puts-volume')
+    graph_x_and_y_cols(puts, option_date, ['strike', 'impliedVolatility'], 'puts-iv')
+    graph_x_and_y_cols(puts, option_date, ['strike', 'volume'], 'puts-volume')
 
     return {
         'ticker': TICKER_NAME,
         'put-call-vol-ratio': (puts['volume'].sum()/calls['volume'].sum()).round(2),
-        'call-volume-range': calculate_vol_for_strike_range(calls),
-        'put-volume-range': calculate_vol_for_strike_range(puts)
+        'call-volume-range': calculate_vol_for_strike_range(calls, 100),
+        'put-volume-range': calculate_vol_for_strike_range(puts, 100)
     }
 
 
 if __name__ == '__main__':
     COLUMNS = ['strike', 'bid', 'ask', 'volume', 'openInterest','impliedVolatility']
     TICKER_NAME = 'TSLA'
+
+    stock = yf.Ticker(TICKER_NAME)
+    option_date = stock.options[0]
+    opt = stock.option_chain(option_date)
     
-    pprint(option_analysis(TICKER_NAME, COLUMNS))
-
-
-
-
-# thoughts:
-
-# sns.distplot(calls['ask'], label = 'impliedVolatility')
-# plt.plot(calls['strike'], calls['volume'])
-# # g = sns.jointplot("strike", "ask", calls, kind='hex')
-# # sns.pairplot(calls)
-# plt.title(f'{option_date}')
-# plt.xlabel('strike')
-# plt.ylabel('volume')
-# plt.savefig(f'calls.png')
-# plt.close()
+    output = option_analysis(option_date, opt, TICKER_NAME, COLUMNS)
+    pprint(output)
