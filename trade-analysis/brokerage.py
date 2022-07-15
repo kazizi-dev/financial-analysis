95cae0f87154fa4d2f33e60b75799a74df3f33e1
kazizi-dev <kouroshazizi2012@gmail.com>
2022-07-14 17:27:26 -0700

    get ticker info using symbol or id

--- trade-analysis/brokerage.py
+++ trade-analysis/brokerage.py
@@ -1,8 +1,5 @@
 import requests
 import config
-from requests.auth import HTTPBasicAuth
-from pprint import pprint
-
 
 
 class Brokerage:
@@ -15,9 +12,22 @@ class Brokerage:
 		pass
 
 	# retrieve historical candle information
-	def get_historical_data(self, id, start_time: str, end_time: str, timeframe: int):
-		interval = config.INTERVALS[timeframe]
+	def get_historical_data(self, id, start_time: str, end_time: str, interval: int):
 		URL = f"{config.API_SERVER}/v1/markets/candles/{id}?startTime={start_time}&endTime={end_time}&interval={interval}"
+		response = requests.get(
+			URL,
+			headers= {
+				'content-type': 'application/json', 
+				'Authorization': config.TOKEN
+			}
+		)
+
+		return response
+
+	
+	# retrieve ticker id based on name
+	def get_ticker_info_using_symbol(self, ticker: str):
+		URL = f"{config.API_SERVER}/v1/symbols/search?prefix={ticker}"
 		response = requests.get(
 			URL,
 			headers= {
@@ -30,7 +40,7 @@ class Brokerage:
 
 
 	# retrieve information about the ticker using its id
-	def get_ticker_info(self, id):
+	def get_ticker_info_using_id(self, id: int):
 		URL = f"{config.API_SERVER}/v1/symbols/{id}"
 		response = requests.get(
 			URL,
